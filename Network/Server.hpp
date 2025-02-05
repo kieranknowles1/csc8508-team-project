c2c80effcceb6986d0326ebbc3f787cb7fbf1eba
Thomas Johnson (UG) <c1031564@newcastle.ac.uk>
2025-02-05 11:22:02 +0000

    Add includes and Client class.

--- Network/Server.hpp
+++ Network/Server.hpp
@@ -7,15 +7,6 @@
 #include <NetworkBase.h>
 
 
-#ifdef NETWORK_TEST
-#include <iostream>
-
-void DebugOut(const std::string& message) {
-	std::cerr << "[DEBUG]: ";
-	std::cerr << message << std::endl;
-}
-
-#endif
 
 
 
