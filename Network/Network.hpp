d50980be777aebef5a40ccd51a1e442abec5a99c
Thomas Johnson (UG) <c1031564@newcastle.ac.uk>
2025-02-05 11:19:43 +0000

    Add channel enum for network channel selection.

--- Network/Network.hpp
+++ Network/Network.hpp
@@ -9,3 +9,17 @@ void DebugOut(const std::string& message) {
 }
 
 #endif
+
+
+/**
+ * @brief Channel enum class.
+ * 
+ * Defines the different channels that the network can use.
+ * @warning Do not assign values to the channels.
+ * 
+ */
+enum class Channel {
+	RELIABLE = 0,	// MUST BE FIRST CHANNEL AS FIRST CHANNEL IS ALWAYS RELIABLE.
+	FREQUENT,		// Used for packet data that can be lost and is frequently sent.
+	CHANNEL_COUNT
+};
