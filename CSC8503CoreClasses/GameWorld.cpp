112cc5c4444fc38675cfe3f40099e6589f73b922
c4072047 <c4072047@18SCOMPD172>
2025-02-14 16:06:53 +0000

    Clean GameWorld: Remove non functioning constraints

--- CSC8503CoreClasses/GameWorld.cpp
+++ CSC8503CoreClasses/GameWorld.cpp
@@ -2,9 +2,7 @@
 #include "GameObject.h"
 #include "Camera.h"
 
-
-using namespace NCL;
-using namespace NCL::CSC8503;
+namespace NCL::CSC8503 {
 
 GameWorld::GameWorld()	{
 	shuffleConstraints	= false;
@@ -19,16 +17,12 @@ GameWorld::~GameWorld()	{
 
 void GameWorld::Clear() {
 	gameObjects.clear();
-	constraints.clear();
 	worldIDCounter		= 0;
 	worldStateCounter	= 0;
 }
 
 void GameWorld::ClearAndErase() {
 	for (auto& i : gameObjects) {
-		delete i;
-	}
-	for (auto& i : constraints) {
 		delete i;
 	}
 	Clear();
@@ -68,75 +62,9 @@ void GameWorld::UpdateWorld(float dt) {
 	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
 	std::default_random_engine e(seed);
 
-	if (shuffleObjects) {
-		std::shuffle(gameObjects.begin(), gameObjects.end(), e);
-	}
-
-	if (shuffleConstraints) {
-		std::shuffle(constraints.begin(), constraints.end(), e);
-	}
-
 	for (auto& i : gameObjects) {
 		i->Update(dt);
 	}
 }
 
-bool GameWorld::Raycast(Ray& r, RayCollision& closestCollision, bool closestObject, GameObject* ignoreThis) const {
-	// TODO: Reimplement for bullet
-	return false;
-	////The simplest raycast just goes through each object and sees if there's a collision
-	//RayCollision collision;
-
-	//for (auto& i : gameObjects) {
-	//	if (!i->GetBoundingVolume()) { //objects might not be collideable etc...
-	//		continue;
-	//	}
-	//	if (i == ignoreThis) {
-	//		continue;
-	//	}
-	//	RayCollision thisCollision;
-	//	if (CollisionDetection::RayIntersection(r, *i, thisCollision)) {
-	//
-	//		if (!closestObject) {
-	//			closestCollision		= collision;
-	//			closestCollision.node = i;
-	//			return true;
-	//		}
-	//		else {
-	//			if (thisCollision.rayDistance < collision.rayDistance) {
-	//				thisCollision.node = i;
-	//				collision = thisCollision;
-	//			}
-	//		}
-	//	}
-	//}
-	//if (collision.node) {
-	//	closestCollision		= collision;
-	//	closestCollision.node	= collision.node;
-	//	return true;
-	//}
-	//return false;
-}
-
-
-/*
-Constraint Tutorial Stuff
-*/
-
-void GameWorld::AddConstraint(Constraint* c) {
-	constraints.emplace_back(c);
-}
-
-void GameWorld::RemoveConstraint(Constraint* c, bool andDelete) {
-	constraints.erase(std::remove(constraints.begin(), constraints.end(), c), constraints.end());
-	if (andDelete) {
-		delete c;
-	}
-}
-
-void GameWorld::GetConstraintIterators(
-	std::vector<Constraint*>::const_iterator& first,
-	std::vector<Constraint*>::const_iterator& last) const {
-	first	= constraints.begin();
-	last	= constraints.end();
 }
