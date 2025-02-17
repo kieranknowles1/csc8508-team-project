4e33effb20cc19800578c21a5ca186e43701db53
c4072047 <c4072047@18SCOMPD172>
2025-02-17 16:44:10 +0000

    Update configure-ps5

--- configure-ps5.bat
+++ configure-ps5.bat
@@ -1,4 +1,4 @@
 mkdir build
 pushd build
-cmake.exe -DCMAKE_TOOLCHAIN_FILE="%SCE_ROOT_DIR%\Prospero\Tools\CMake\PS5.cmake" -A Prospero %*  -DDEBUG_ASSET_ROOT:STRING=/app0/Assets/ ..
+cmake.exe -DCMAKE_TOOLCHAIN_FILE="%SCE_ROOT_DIR%\Prospero\Tools\CMake\PS5.cmake" -DPS5_BUILD=ON -A Prospero %*  -DDEBUG_ASSET_ROOT:STRING=/app0/Assets/ ..
 popd
