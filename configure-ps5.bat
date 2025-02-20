b84dde4846b8c2ce6c08407119aff94a402076b5
c4072047 <c4072047@18SCOMPD172>
2025-02-20 13:51:10 +0000

    Don't build asset path into the executable

--- configure-ps5.bat
+++ configure-ps5.bat
@@ -1,6 +1,6 @@
 mkdir build-ps5
 pushd build-ps5
-cmake.exe -DCMAKE_TOOLCHAIN_FILE="%SCE_ROOT_DIR%\Prospero\Tools\CMake\PS5.cmake" -DPS5_BUILD=ON -A Prospero %*  -DDEBUG_ASSET_ROOT:STRING=/app0/Assets/ ..
+cmake.exe -DCMAKE_TOOLCHAIN_FILE="%SCE_ROOT_DIR%\Prospero\Tools\CMake\PS5.cmake" -DPS5_BUILD=ON -A Prospero %* ..
 popd
 
 @REM# FIXME: Manual steps required:
