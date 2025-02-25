50615e3eb9abb1744d0876b247127580debc14e9
Thomas Johnson (UG) <c1031564@newcastle.ac.uk>
2025-02-25 15:19:04 +0000

    Create packets for position and player changing state.

--- TeamProject/Multiplayer/GamePackets.hpp
+++ TeamProject/Multiplayer/GamePackets.hpp
@@ -1,12 +1,18 @@
 #pragma once
 
+#include <bullet/LinearMath/btVector3.h>
+#include <bullet/LinearMath/btQuaternion.h>
+
 #include "Network/Network.hpp"
-#include "bullet/LinearMath/btVector3.h"
+#include "PlayerObject.h"
 
 
 namespace Packet {
 	const Type DELTA = CUSTOM_TYPE;
 	const Type POSITION = CUSTOM_TYPE + 1;
+	const Type PLAYER_DEATH = CUSTOM_TYPE + 2;
+	const Type PLAYER_SPAWN = CUSTOM_TYPE + 3;
+	const Type PLAYER_GRAVITY_CHANGE = CUSTOM_TYPE + 4;
 
 
 	/**
@@ -26,6 +32,45 @@ namespace Packet {
 		int m_objectID;
 		btVector3 m_linearVelocity;
 		btVector3 m_angularVelocity;
+	};
+
+
+	/**
+	 * @brief Position Packet class.
+	 * 
+	 * Stores the position and orientation of the object.
+	 */
+	class PositionPacket : public Packet {
+	public:
+		PositionPacket(int objectID, const btVector3& position, const btQuaternion& orientation, int sequenceNum) :
+			Packet(DELTA, static_cast<int>(Channel::FREQUENT), sequenceNum),
+			m_objectID(objectID), m_position(position), m_orientation(orientation)
+		{}
+
+	private:
+		int m_objectID;
+		btVector3 m_position;
+		btQuaternion m_orientation;
+	};
+
+
+	/**
+	 * @brief Player Change State Packet class.
+	 * 
+	 * Used to notify of a player death.
+	 */
+	class PlayerChangeStatePacket : public Packet {
+	public:
+		PlayerChangeStatePacket(int objectID, int playerID, PlayerState state, int sequenceNum) :
+			Packet(DELTA, static_cast<int>(Channel::UNSEQUENCED), sequenceNum),
+			m_objectID(objectID), m_playerID(playerID), m_newState(state)
+		{}
+
+	private:
+		int m_objectID;
+		int m_playerID;
+		PlayerState m_newState;
+
 	};
 }
 
