a47f94000e536fb4285fef6c8ae7e677e988ef26
Thomas Johnson (UG) <c1031564@newcastle.ac.uk>
2025-02-26 09:06:27 +0000

    Add Handler Classes for Custom Packets.

--- TeamProject/Multiplayer/GamePacketHandlers.hpp
+++ TeamProject/Multiplayer/GamePacketHandlers.hpp
@@ -1,100 +1,45 @@
 #pragma once
 
-#include <bullet/LinearMath/btVector3.h>
-#include <bullet/LinearMath/btQuaternion.h>
-
-#include "Network/Network.hpp"
-#include "PlayerObject.h"
-
+#include "GamePackets.hpp"
 
 namespace Packet {
-	/**
-	 * @brief PacketType Enum.
-	 * 
-	 * Contains user defined enums for the types of packets in the network.
-	 */
-	enum class PacketType : Type {
-		DELTA = CUSTOM_TYPE,
-		POSITION = CUSTOM_TYPE + 1,
-		PLAYER_STATE_CHANGE = CUSTOM_TYPE + 2,
-		OBJECT_CHANGE_GRAVITY = CUSTOM_TYPE + 3
+
+	class DeltaPacketHandler : public PacketHandler {
+	public:
+		DeltaPacketHandler() : PacketHandler(static_cast<Type>(PacketType::DELTA)) {}
+
+		void Handle(Packet packet) override;
+		Packet Translate(ENetPacket* packet) override;
+		ENetPacket* ToENetPacket(const Packet& packet) override;
 	};
 
 
-	/**
-	 * @brief Delta Packet class.
-	 *
-	 * Stores the linear and angular velocity of an object.
-	 * Used by clients for prediction of object positions.
-	 */
-	class DeltaPacket : public Packet {
+	class PositionPacketHandler : public PacketHandler {
 	public:
-		DeltaPacket(int objectID, const btVector3& linear, const btVector3& angular, int sequenceNum) :
-			Packet(static_cast<Type>(PacketType::DELTA), static_cast<int>(Channel::FREQUENT), sequenceNum),
-			m_objectID(objectID), m_linearVelocity(linear), m_angularVelocity(angular)
-		{}
+		PositionPacketHandler() : PacketHandler(static_cast<Type>(PacketType::POSITION)) {}
 
-	private:
-		int m_objectID;
-		btVector3 m_linearVelocity;
-		btVector3 m_angularVelocity;
+		void Handle(Packet packet) override;
+		Packet Translate(ENetPacket* packet) override;
+		ENetPacket* ToENetPacket(const Packet& packet) override;
 	};
 
 
-	/**
-	 * @brief Position Packet class.
-	 * 
-	 * Stores the position and orientation of the object.
-	 */
-	class PositionPacket : public Packet {
+	class PlayerStateChangePacketHandler : public PacketHandler {
 	public:
-		PositionPacket(int objectID, const btVector3& position, const btQuaternion& orientation, int sequenceNum) :
-			Packet(static_cast<Type>(PacketType::POSITION), static_cast<int>(Channel::FREQUENT), sequenceNum),
-			m_objectID(objectID), m_position(position), m_orientation(orientation)
-		{}
+		PlayerStateChangePacketHandler() : PacketHandler(static_cast<Type>(PacketType::PLAYER_STATE_CHANGE)) {}
 
-	private:
-		int m_objectID;
-		btVector3 m_position;
-		btQuaternion m_orientation;
+		void Handle(Packet packet) override;
+		Packet Translate(ENetPacket* packet) override;
+		ENetPacket* ToENetPacket(const Packet& packet) override;
 	};
 
 
-	/**
-	 * @brief Player Change State Packet class.
-	 * 
-	 * Used to notify of a player's state change (usually spawning in or
-	 * dying).
-	 */
-	class PlayerChangeStatePacket : public Packet {
+	class ObjectChangeGravityPacketHandler : public PacketHandler {
 	public:
-		PlayerChangeStatePacket(int playerID, const PlayerState& state, int sequenceNum) :
-			Packet(static_cast<Type>(PacketType::PLAYER_STATE_CHANGE), static_cast<int>(Channel::UNSEQUENCED), sequenceNum),
-			m_playerID(playerID), m_newState(state)
-		{}
+		ObjectChangeGravityPacketHandler() : PacketHandler(static_cast<Type>(PacketType::OBJECT_CHANGE_GRAVITY)) {}
 
-	private:
-		int m_playerID;
-		PlayerState m_newState;
-	};
-
-
-	/**
-	 * @brief Objet Change Gravity Packet class.
-	 * 
-	 * Used to change the direction of an objects gravity.
-	 * Changes the up vector of the object.
-	 */
-	class ObjectChangeGravity : public Packet {
-	public:
-		ObjectChangeGravity(int objectID, const btVector3& upDirection, int sequenceNum) :
-			Packet(static_cast<Type>(PacketType::OBJECT_CHANGE_GRAVITY), static_cast<int>(Channel::UNSEQUENCED), sequenceNum),
-			m_objectID(objectID), m_upVector(upDirection)
-		{}
-
-	private:
-		int m_objectID;
-		btVector3 m_upVector;
+		void Handle(Packet packet) override;
+		Packet Translate(ENetPacket* packet) override;
+		ENetPacket* ToENetPacket(const Packet& packet) override;
 	};
 }
-
