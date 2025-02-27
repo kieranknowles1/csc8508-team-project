8923dbb1f6f11b6aa1d7b81ab84270d33a6dcc06
Thomas Johnson (UG) <c1031564@newcastle.ac.uk>
2025-02-27 10:23:29 +0000

    Add serialization and deserialization for position packets.

--- TeamProject/Multiplayer/GamePacketHandlers.cpp
+++ TeamProject/Multiplayer/GamePacketHandlers.cpp
@@ -92,11 +92,72 @@ namespace Packet {
 	}
 
 	std::shared_ptr<Packet> PositionPacketHandler::Translate(const ENetPacket* packet) const {
-		return std::make_shared<Packet>();
+		Type type;
+		uint8_t channel;
+		uint32_t sequenceNumber;
+		
+		int objectID;
+		btVector3 position;
+		btQuaternion orientation;
+		size_t offset = sizeof(Type) + sizeof(uint8_t) + sizeof(uint32_t);
+
+		GetBaseData(packet, &type, &channel, &sequenceNumber);
+
+		memcpy(&objectID, packet->data + offset, sizeof(int));
+		offset += sizeof(int);
+
+		memcpy(&position, packet->data + offset, sizeof(btVector3));
+		offset += sizeof(btVector3);
+
+		memcpy(&orientation, packet->data + offset, sizeof(btQuaternion));
+		offset += sizeof(btQuaternion);
+
+		return std::make_shared<PositionPacket>(objectID, position, orientation, sequenceNumber);
 	}
 
 	ENetPacket* PositionPacketHandler::ToENetPacket(const std::shared_ptr<Packet> packet) const {
-		return nullptr;
+		char* buffer = new char[
+			sizeof(Type)
+			+ sizeof(uint8_t)
+			+ sizeof(uint32_t)
+			+ sizeof(int)
+			+ sizeof(btVector3)
+			+ sizeof(btQuaternion)
+		];
+
+		PositionPacket positionPacket = (*static_cast<PositionPacket*>(packet.get()));
+		size_t offset = 0;
+
+		Type type = positionPacket.GetType();
+		memcpy(buffer, &type, sizeof(Type));
+		offset = offset + sizeof(Type);
+
+		uint8_t channel = positionPacket.GetChannel();
+		memcpy(buffer + offset, &channel, sizeof(uint8_t));
+		offset = offset + sizeof(uint8_t);
+
+		uint32_t sequenceNumber = positionPacket.GetSequenceNumber();
+		memcpy(buffer + offset, &sequenceNumber, sizeof(uint32_t));
+		offset = offset + sizeof(uint32_t);
+
+		int objectID = positionPacket.GetTargetID();
+		memcpy(buffer + offset, &objectID, sizeof(int));
+		offset = offset + sizeof(int);
+
+		btVector3 position = positionPacket.GetPosition();
+		memcpy(buffer + offset, &position, sizeof(btVector3));
+		offset = offset + sizeof(btVector3);
+
+		btQuaternion orientation = positionPacket.GetOrientation();
+		memcpy(buffer + offset, &orientation, sizeof(btQuaternion));
+		offset = offset + sizeof(btQuaternion);
+
+		int packetFlags = 0;
+		if (channel == static_cast<int>(Channel::RELIABLE)) packetFlags = ENET_PACKET_FLAG_RELIABLE;
+		else if (channel == static_cast<int>(Channel::UNSEQUENCED)) packetFlags = ENET_PACKET_FLAG_UNSEQUENCED;
+
+		ENetPacket* enetPacket = enet_packet_create(buffer, offset, packetFlags);
+		return enetPacket;
 	}
 
 
