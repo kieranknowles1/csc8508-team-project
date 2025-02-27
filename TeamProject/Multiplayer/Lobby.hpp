6fce1641ded0bec21d09a1443bbc48a40c4f6e78
Thomas Johnson <thomas27johnson39@gmail.com>
2025-02-27 16:21:57 +0000

    Add Lobby class.

--- TeamProject/Multiplayer/Lobby.hpp
+++ TeamProject/Multiplayer/Lobby.hpp
@@ -1,1 +1,25 @@
 #pragma once
+
+#include <unordered_map>
+
+#include "Multiplayer/User.hpp"
+
+namespace Lobbies {
+	/**
+	 * @brief Lobby class that contains a collection of user IDs to separate
+	 * who is who.
+	 */
+	class Lobby {
+	public:
+		/**
+		 * @brief Construct Lobby with host User.
+		 * 
+		 * @param hostID The User who is in charge of the lobby.
+		 */
+		Lobby(User hostUser) : m_hostUser(hostUser) {}
+
+	private:
+		User m_hostUser;
+		std::unordered_map<int, User> m_players;
+	};
+}
