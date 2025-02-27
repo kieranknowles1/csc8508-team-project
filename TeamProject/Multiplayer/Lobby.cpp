0e58c3648a1aa1dddaa05db7c64de7603556a4ba
Thomas Johnson <thomas27johnson39@gmail.com>
2025-02-27 21:26:13 +0000

    Add UserColor and add/remove users from lobby.

--- TeamProject/Multiplayer/Lobby.cpp
+++ TeamProject/Multiplayer/Lobby.cpp
@@ -1,1 +1,19 @@
 #include "Lobby.hpp"
+
+
+
+namespace Lobbies {
+	bool UserColor::SetUser(User user) {
+		if (m_assigned.has_value()) return false;
+		m_assigned.emplace(user);
+	}
+
+
+
+	bool Lobby::AddUser(User user) {
+		if (m_players.size() >= m_maxSize) return false;
+		m_players.insert(std::make_pair(user.GetUserID(), user));
+	}
+
+
+}
--- TeamProject/Multiplayer/Lobby.hpp
+++ TeamProject/Multiplayer/Lobby.hpp
@@ -3,8 +3,51 @@
 #include <unordered_map>
 
 #include "Multiplayer/User.hpp"
+#include "Colors.h"
 
 namespace Lobbies {
+	/**
+	 * @brief Color Selection Class.
+	 * 
+	 * Assigns a user to the color. Only one user can be assigned.
+	 */
+	class UserColor {
+	public:
+		/**
+		 * @brief UserColor Constructor.
+		 * 
+		 * @param color The color this UserColor will represent.
+		 */
+		UserColor(TeamColor color) : m_color(color) {}
+
+		/**
+		 * @brief Assign a user to be in control of this player.
+		 * 
+		 * If there is already a user in control, this function will not allow
+		 * the new player to be assigned. The old user must be removed first.
+		 * 
+		 * @param user The user to assume control.
+		 * @return true if the user was allowed to be assigned, otherwise false.
+		 */
+		bool SetUser(User user);
+
+		/**
+		 * @brief Remove the current user's control.
+		 */
+		inline void Clear() { m_assigned.reset(); }
+
+		/**
+		 * @brief Get the user in control.
+		 * 
+		 * @return std::optional container. If there is a user assigned, it will be inside.
+		 */
+		inline std::optional<User> GetUser() { return m_assigned; }
+
+	private:
+		TeamColor m_color;
+		std::optional<User> m_assigned;
+	};
+
 	/**
 	 * @brief Lobby class that contains a collection of user IDs to separate
 	 * who is who.
@@ -16,10 +59,36 @@ namespace Lobbies {
 		 * 
 		 * @param hostID The User who is in charge of the lobby.
 		 */
-		Lobby(User hostUser) : m_hostUser(hostUser) {}
+		Lobby(User hostUser, int maxSize) : m_hostUser(hostUser), m_maxSize(maxSize) {}
+
+		/**
+		 * Fetch the maximum size of the lobby.
+		 * 
+		 * @return The maximum size of the lobby.
+		 */
+		inline int GetLobbySize() const { return m_maxSize; }
+
+		/**
+		 * @brief Add a user to the lobby.
+		 * @param user The user to insert.
+		 * @return true if the user was added, otherwise false.
+		 */
+		bool AddUser(User user);
+
+		/**
+		 * @brief Remove a user from the lobby.
+		 * 
+		 * If the user is not in the lobby, this function does NOT throw an
+		 * exception.
+		 * 
+		 * @param user The user to remove.
+		 */
+		inline void RemoveUser(User* user) { m_players.erase(user->GetUserID()); }
 
 	private:
 		User m_hostUser;
-		std::unordered_map<int, User> m_players;
+		const int m_maxSize;
+
+		std::unordered_map<unsigned int, User> m_players;
 	};
 }
