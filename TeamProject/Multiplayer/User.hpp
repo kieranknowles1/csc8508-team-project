23ac99762016a07f760aa5898e443a8cea731e22
Thomas Johnson <thomas27johnson39@gmail.com>
2025-02-27 16:21:48 +0000

    Comment User class.

--- TeamProject/Multiplayer/User.hpp
+++ TeamProject/Multiplayer/User.hpp
@@ -3,8 +3,20 @@
 class Network;
 
 namespace Lobbies {
+
+
+	/**
+	 * @brief User class containing information about players on the network.
+	 */
 	class User {
 	public:
+		/**
+		 * @brief User Constructor.
+		 * 
+		 * Assigns the userID to the User.
+		 * 
+		 * @param userID the ID the user will use.
+		 */
 		User(unsigned int userID) : m_userID(userID) {}
 	private:
 		unsigned int m_userID = -1;
