c6687f8dfb3da095b6a589d91bfb0f1655e4cece
WJConnors <37446973+WJConnors@users.noreply.github.com>
2025-02-27 15:23:09 +0000

    Changed function name because it no longer returned a boolean

--- TeamProject/Wanderer.h
+++ TeamProject/Wanderer.h
@@ -22,7 +22,7 @@ namespace NCL {
 			btVector3 navOffset;
 
 			StateMachine* stateMachine;
-			void canSeePlayer();
+			void UpdatePlayerDistance();
 			float senseDistance = 100.0f;
 			float playerDist = 100.0f;
 
