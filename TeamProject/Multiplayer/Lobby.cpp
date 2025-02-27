16c59433ba61e5b3f32c30fc5e38507eead9eee2
Thomas Johnson <thomas27johnson39@gmail.com>
2025-02-27 21:56:44 +0000

    Format Files.

--- TeamProject/Multiplayer/Lobby.cpp
+++ TeamProject/Multiplayer/Lobby.cpp
@@ -1,7 +1,5 @@
 #include "Lobby.hpp"
 
-
-
 namespace Lobbies {
 	bool UserColor::SetUser(const User& user) {
 		if (m_assigned.has_value()) return false;
@@ -15,7 +13,6 @@ namespace Lobbies {
 			return UserColor(static_cast<TeamColor>(value++));
 			});
 	}
-
 
 	bool Lobby::AddUser(const User& user) {
 		if (m_players.size() >= m_maxSize) return false;
