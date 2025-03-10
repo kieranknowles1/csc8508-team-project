163b325834dd66e235280727a042d2663607160d
c4072047 <c4072047@18SCOMPD172>
2025-03-10 13:45:04 +0000

    Don't duplicate VS_OUTPUT declarations

--- Assets/Shaders/PSSL/types.h
+++ Assets/Shaders/PSSL/types.h
@@ -1,6 +1,12 @@
 #ifndef PSSL_TYPES_H
 #define PSSL_TYPES_H
 
+#ifdef __cplusplus
+#define SLOT(slot)
+#else
+#define SLOT(slot) : slot
+#endif
+
 #include "interop.h"
 
 #define NULLTEX -1
@@ -18,6 +24,31 @@ struct UiState {
     float2 position;
     float2 size;
     int texture;
+};
+
+struct VS_OUTPUT
+{
+	float4 Position SLOT(S_POSITION);
+	float2 UV SLOT(TEXCOORD0);
+	float4 Colour SLOT(TEXCOORD1);
+	int	texID SLOT(TEXCOORD2);
+
+	float4 ShadowClip SLOT(TEXCOORD2);
+};
+
+struct UI_VS_OUTPUT {
+    float4 position SLOT(S_POSITION);
+    float2 UV SLOT(TEXCOORD0);
+    float4 colour SLOT(TEXCOORD1);
+    int texId SLOT(TEXCOORD2);
+};
+
+struct SHADOW_VS_OUTPUT
+{
+	float4 Position SLOT(POSITION);
+	float2 UV SLOT(TEXCOORD0);
+	float4 Colour SLOT(TEXCOORD1);
+	int	texID SLOT(TEXCOORD2);
 };
 
 #endif
