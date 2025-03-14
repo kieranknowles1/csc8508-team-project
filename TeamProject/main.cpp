349ca0455136fca86351107e3c779f28ed75f3a6
Thomas Johnson <thomas27johnson39@gmail.com>
2025-03-14 21:27:14 +0000

    Menu now doesnt shift when selecting.

--- TeamProject/main.cpp
+++ TeamProject/main.cpp
@@ -288,6 +288,7 @@ int main(int argc, char** argv) {
             for (int i = 0; i < 3; i++) {
                 std::string currentItem = menuItems[i];
                 if (i == selection) currentItem = "> " + currentItem + " <";
+                else currentItem = "  " + currentItem;
                 Debug::Print(currentItem, Vector2(0.4f, 0.35f + (0.1f * i)));
 
             }
