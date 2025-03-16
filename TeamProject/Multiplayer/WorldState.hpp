854b38232749e05d7bd3b52de332813db55ee6b2
Thomas Johnson <thomas27johnson39@gmail.com>
2025-03-16 01:09:51 +0000

    Add function for updating multiple states.

--- TeamProject/Multiplayer/WorldState.hpp
+++ TeamProject/Multiplayer/WorldState.hpp
@@ -8,14 +8,12 @@
 namespace WorldState {
     using StateValue = std::variant<btVector3>;
 
-
     enum class StateType {
         LinearVelocity,
         AngularVelocity,
         Position,
         UpVector
     };
-
 
     /**
      * @brief A thread safe class for storing different states.
@@ -38,10 +36,20 @@ namespace WorldState {
          * @brief Update a state.
          * This function blocks all reading until it has finished.
          */
-        template <typename T>
-        void UpdateState(StateType type, T value) {
+        void UpdateState(StateType type, StateValue value) {
             std::unique_lock lock(m_stateLock);
             m_states[type] = value;
+        }
+
+        /**
+         * @brief Write multiple states updates at once.
+         * Assumes state and value parameters have the same number of items.
+         */
+        void UpdateStates(std::vector<StateType>& states, std::vector<StateValue>& values) {
+            std::unique_lock lock(m_stateLock);
+            for (int i = 0; i < states.size(); i++) {
+                m_states[states[i]] = values[i];
+            }
         }
 
         template <typename T>
