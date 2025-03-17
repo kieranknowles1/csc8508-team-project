04050dbea0f91a4a9a2833ad8a7c42b97b3659be
A-Barnett <c0096097@newcastle.ac.uk>
2025-03-17 12:02:02 +0000

    lock-fix

--- TeamProject/main.cpp
+++ TeamProject/main.cpp
@@ -106,7 +106,7 @@ int main(int argc, char** argv) {
     auto window = createWindow(config);
 
     window->ShowOSPointer(false);
-    window->LockMouseToWindow(false);
+    window->LockMouseToWindow(true);
 
     auto renderer = createRenderer(window.get());
     auto controller = createController(window.get());
