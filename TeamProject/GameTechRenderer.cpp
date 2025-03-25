66852426f3d97fe56775f15efb75261d563a3656
A-Barnett <91531818+A-Barnett@users.noreply.github.com>
2025-03-25 13:02:49 +0000

    Added centre model transform

--- TeamProject/GameTechRenderer.cpp
+++ TeamProject/GameTechRenderer.cpp
@@ -1288,6 +1288,7 @@ void GameTechRenderer::RenderAnimations() {
 			Matrix4 modelMatrix; 
 			i->getParent()->GetTransform().getOpenGLMatrix((btScalar*)&modelMatrix);
 			modelMatrix = modelMatrix * Matrix::Scale(i->getParent()->getRenderScale());
+			modelMatrix = modelMatrix * Matrix::Translation(Vector3(0, -0.9f, 0.1f));
 			glUniformMatrix4fv(modelLocation, 1, false, (float*)&modelMatrix);
 
 			Vector4 Colour = i->GetColour();
