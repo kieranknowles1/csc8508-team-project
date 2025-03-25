e17c1db52701f00cee951e468a8c1fa71b9a7abe
GrandIguana <m.l.davidson1@newcastle.ac.uk>
2025-03-25 16:53:55 +0000

    Added faster strafe animations

--- TeamProject/AnimationObject.cpp
+++ TeamProject/AnimationObject.cpp
@@ -7,12 +7,12 @@ using namespace CSC8503;
 AnimationObject::AnimationObject(GameObject* parent) {
     animations[JUMPING_SPRINT] = new MeshAnimation("/RacerGuy/RunningJump.anm");
     animations[JUMPING_STANDING] = new MeshAnimation("/RacerGuy/WeaponJump.anm");
-    animations[SLIDING] = new MeshAnimation("/RacerGuy/AIOSlideNoRM.anm");
+    animations[SLIDING] = new MeshAnimation("/RacerGuy/WeaponIdle.anm");
     animations[FALLING] = new MeshAnimation("/RacerGuy/FallingIdle.anm");
     animations[SPRINTING_FORWARD] = new MeshAnimation("/RacerGuy/WeaponRun.anm");
     animations[SPRINTING_BACK] = new MeshAnimation("/RacerGuy/WeaponRunBackwards.anm");
-    animations[SPRINTING_LEFT] = new MeshAnimation("/RacerGuy/StrafeLeft.anm");
-    animations[SPRINTING_RIGHT] = new MeshAnimation("/RacerGuy/StrafeRight.anm");
+    animations[SPRINTING_LEFT] = new MeshAnimation("/RacerGuy/FastStrafeLeft.anm");
+    animations[SPRINTING_RIGHT] = new MeshAnimation("/RacerGuy/FastStrafeRight.anm");
     animations[WALKING_FORWARD] = new MeshAnimation("/RacerGuy/WeaponWalk.anm");
     animations[WALKING_BACK] = new MeshAnimation("/RacerGuy/WeaponWalkBackwards.anm");
     animations[WALKING_LEFT] = new MeshAnimation("/RacerGuy/StrafeLeft.anm");
